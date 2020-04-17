7eae7aac6d574a05c64e6d3d718d5c9e62ee698f
emilyanas2323 <32170897+emilyanas2323@users.noreply.github.com>
2020-04-16 20:55:17 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,13 @@
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "eanas@edu.uwaterloo.ca";
+$formcontent="From: $name \nMessage: $message";
+$recipient = "emilyihabanas@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $message) or die("Error!");
+$mailheader = "From: $email";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+/*echo "Name: $name <br>";
+echo "Email: $email <br>";
+echo "Message: $message <br>";*/
 echo "Thank You!";
 ?>
