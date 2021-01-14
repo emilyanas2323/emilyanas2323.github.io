4b0b55481386c965a151ada786469676fc4a0b66
emilyanas2323 <32170897+emilyanas2323@users.noreply.github.com>
2021-01-13 22:10:44 -0500

    Cloned and updated website

---
+++ _site/assets/js/cat-generator/game-preview.js
@@ -0,0 +1,245 @@
+const spawn = document.getElementById('spawn');
+const destroy = document.getElementById('destroy');
+
+var catImage = new Image(); 
+const catImages = ["/assets/images/cat-generator/catImage.png"];
+catImage.src = catImages[0];
+
+var backgroundImage = new Image();
+const backgroundImages = ["/assets/images/cat-generator/space.png"];
+backgroundImage.src = backgroundImages[0];
+
+var catWidth = catImage.naturalWidth/6.5;
+var catHeight = catImage.naturalHeight/6.5;
+
+/*const catImg = document.getElementById('catImg');
+const catImg1 = document.getElementById('catImg1');
+const catImg2 = document.getElementById('catImg2');
+const catImg3 = document.getElementById('catImg3');
+
+const space = document.getElementById('space');
+const sky = document.getElementById('sky');
+const city = document.getElementById('city');
+const snowy = document.getElementById('snowy');
+
+var rangeslider = document.getElementById("sliderRange");
+rangeslider.value = "5.5";*/
+var catSpeed = 2.75;
+
+
+function cat(x, y, xDir, yDir) {
+    this.x = x;
+    this.y = y;
+    this.width = catWidth;
+    this.height = catHeight;
+    this.xDir = -1 * xDir * catSpeed;
+	this.yDir = -1 * yDir * catSpeed;
+}
+
+cat.prototype.update = function () {
+    if( this.y <= 0 || this.y+this.height*1.2 >= game.gameFieldHeight()) {
+        this.yDir *= -1;
+		while(this.y <= 0 || this.y+this.height*1.2 >= game.gameFieldHeight()) {
+			this.y += this.yDir;
+		}
+    }
+	if( this.x <= 0 || this.x+this.width*1.2 >= game.gameFieldWidth()) {
+        this.xDir *= -1;
+		while(this.x <= 0 || this.x+this.width*1.2 >= game.gameFieldWidth()) {
+			this.x += this.xDir;
+		}
+    }
+};
+
+
+var renderer = (function () {
+
+    function _drawcat(context, cat) {
+        context.drawImage(catImage,cat.x,cat.y,catWidth*1.2,catHeight*1.2);
+    }
+
+    function _render() {
+        var canvas = document.getElementById("game-layer");
+        var context = canvas.getContext("2d");
+
+        /*context.fillStyle = "pink";
+        context.fillRect(0, 0, canvas.width, canvas.height);*/
+		context.drawImage(backgroundImage,0,0,canvas.width, canvas.height);
+
+        var i,
+            entity,
+            entities = game.entities();
+
+        for (i=0; i < entities.length; i++) {
+            entity = entities[i];
+			_drawcat(context, entity);
+        }
+    }
+
+    return {
+        render: _render
+    };
+
+})();
+
+
+var physics = (function () {
+
+    function _update() {
+        var i, j, k, m, entities = game.entities();
+
+		// move cats based on x,y Dirs
+        for( i=0; i<entities.length; i++) {
+            entities[i].y += entities[i].yDir;
+			entities[i].x += entities[i].xDir;
+        }
+		
+		// check for collisions
+		for(j=0; j<entities.length; j++){
+			for(k=0;k<entities.length;k++){
+				if(k > j){
+						if(entities[j].x < entities[k].x + entities[k].width && entities[j].x + entities[j].width > entities[k].x && entities[j].y < entities[k].y + entities[k].height && entities[j].height + entities[j].y > entities[k].y){
+							entities[j].xDir *= -1;
+							entities[j].x += entities[j].xDir * 2;
+							entities[j].yDir *= -1;
+							entities[j].y += entities[j].yDir * 2;
+							entities[k].xDir *= -1;
+							entities[k].x += entities[k].xDir * 2;
+							entities[k].yDir *= -1;
+							entities[k].y += entities[k].yDir * 2;
+						}
+				}
+			}
+		}
+		
+		/*rangeslider.oninput = function() {
+			catSpeed = this.value * 0.5;
+			for(m=0; m<entities.length; m++){
+				if(entities[m].yDir < 0){
+					entities[m].yDir = -1 * catSpeed;
+				}
+				else{
+					entities[m].yDir = 1 * catSpeed;
+				}
+				if(entities[m].xDir < 0){
+					entities[m].xDir = -1 * catSpeed;
+				}
+				else{
+					entities[m].xDir = 1 * catSpeed;
+				}
+			}
+		}*/
+    }
+
+    return {
+        update: _update
+    };
+
+})();
+
+
+var game = (function () {
+	var _gameFieldHeight = 300;
+	var _gameFieldWidth = 405;
+    var _entities = [];
+
+    function _start() {
+		_entities.push(new cat(Math.floor(Math.random() * (_gameFieldWidth - catWidth)), Math.floor(Math.random() * (_gameFieldHeight - catHeight)),1,1));
+
+        window.requestAnimationFrame(this.update.bind(this));
+    }
+
+    function _update() {
+        physics.update();
+
+        var i;
+        for( i=0; i<_entities.length; i++) {
+            _entities[i].update();
+        }
+
+        renderer.render();
+
+        window.requestAnimationFrame(this.update.bind(this));
+    }
+	
+	spawn.addEventListener('click', () => {
+		if(_entities.length < 8){
+			var collision = false;
+			do {
+				collision = false;
+				var randomX = Math.floor(Math.random() * (_gameFieldWidth - catWidth));
+				var randomY = Math.floor(Math.random() * (_gameFieldHeight - catHeight));
+				for(var i = 0; i<_entities.length; i++){
+					if(_entities[i].x < randomX + catWidth && _entities[i].x + _entities[i].width > randomX && _entities[i].y < randomY + catHeight && _entities[i].height + _entities[i].y > randomY) {
+						collision = true;
+					}
+				}
+			} while (collision);
+			
+			if(_entities.length % 4 == 0) {
+				_entities.push(new cat(randomX, randomY, 1, 1));
+			}
+			else if(_entities.length % 4 == 1) {
+				_entities.push(new cat(randomX, randomY, -1, 1));
+			}
+			else if(_entities.length % 4 == 2) {
+				_entities.push(new cat(randomX, randomY, 1, -1));
+			}
+			else {
+				_entities.push(new cat(randomX, randomY, -1, -1));
+			}
+		}
+	})
+	
+	destroy.addEventListener('click', () => {
+		if(_entities.length > 0){
+			_entities.pop();
+		}
+	})
+	
+	/*catImg.addEventListener('click', () => {
+		catImage.src = catImages[0];
+	})
+	
+	catImg1.addEventListener('click', () => {
+		catImage.src = catImages[1];
+	})
+	
+	catImg2.addEventListener('click', () => {
+		catImage.src = catImages[2];
+	})
+	
+	catImg3.addEventListener('click', () => {
+		catImage.src = catImages[3];
+	})
+	
+	space.addEventListener('click', () => {
+		backgroundImage.src = backgroundImages[0];
+	})
+	
+	sky.addEventListener('click', () => {
+		backgroundImage.src = backgroundImages[1];
+	})
+	
+	city.addEventListener('click', () => {
+		backgroundImage.src = backgroundImages[2];
+	})
+	
+	snowy.addEventListener('click', () => {
+		backgroundImage.src = backgroundImages[3];
+	})*/
+
+    return {
+        start: _start,
+        update: _update,
+		entities: function () { return _entities; },
+		gameFieldWidth: function () { return _gameFieldWidth; },
+        gameFieldHeight: function () { return _gameFieldHeight; }
+    };
+
+})();
+
+window.onload = function(e){ 
+	game.start();
+};
+
---
+++ _site/assets/js/webpage-editor/webpage-editor.js
@@ -0,0 +1,353 @@
+let website = document.getElementById('website');
+let siteSections = $("#section-container").children();
+let panelSections = $("#accordion").children();
+let textElems = document.getElementsByClassName("text-elem");
+let linkElems = document.getElementsByClassName("link-elem");
+
+/* Testing */
+$( document ).ready(function() {
+    //console.log(document.getElementById("header-title"));
+});
+
+/***********************************************************************************************************************************************/
+// add title
+/*let makeHeader = document.getElementById('add-header')
+makeHeader.addEventListener("click", evt => {
+    // creating title element
+    var title = document.createElement("h3");
+    title.id = "titleee";
+    title.appendChild(document.createTextNode("New title"));
+
+    // creating desc element
+    var desc = document.createElement("p");
+    desc.id = "desc";
+    desc.appendChild(document.createTextNode("New description"));
+
+    // creating button element
+    var button = document.createElement("button");
+    button.id = "btn";
+    button.appendChild(document.createTextNode("New button"));
+
+    // adding new elemements to DOM
+    website.appendChild(title);
+    website.appendChild(desc);
+    website.appendChild(button);
+},{once:true});*/
+
+// add text
+/*let makeText = document.getElementById('add-text')
+makeText.addEventListener("click", evt => {
+    // creating text element
+    var text = document.createElement("p");
+    text.id = "texttt";
+    text.appendChild(document.createTextNode("New text"));
+    website.appendChild(text);
+},{once:true});*/
+
+
+/***********************************************************************************************************************************************/
+/* Section editor controls */
+
+function getIndexOfThis(item,array) {
+    var index;
+    for (var i = 0; i < array.length; i++) {
+        if(array[i] == item){
+            index = i;
+        }
+    }
+    return index;
+}
+
+/* Content */
+let textValues = document.getElementsByClassName("text-value");
+let textChange = document.getElementsByClassName("text-change");
+$(".text-change").click(function() {
+    var index = getIndexOfThis(this,textChange);
+    textElems[index].textContent = textValues[index].value;
+});
+
+let linkValues = document.getElementsByClassName("link-value");
+let linkChange = document.getElementsByClassName("link-change");
+$(".link-change").click(function() {
+    var index = getIndexOfThis(this,linkChange);
+    var url = linkValues[index].value;
+    var pattern = /^((http|https|ftp):\/\/)/;
+    if(!pattern.test(url)) {
+        url = "https://" + url;
+    }
+    linkElems[index].href = url;
+});
+
+/* Styling */
+$(".bold-btn").click(function(){
+    var boldButtons = Array.prototype.slice.call(document.getElementsByClassName("bold-btn"));
+    var index = boldButtons.indexOf(this);
+    textElems[index].classList.toggle('bold-text');
+});
+
+$(".italic-btn").click(function(){
+    var italicButtons = Array.prototype.slice.call(document.getElementsByClassName("italic-btn"));
+    var index = italicButtons.indexOf(this);
+    textElems[index].classList.toggle('italics-text');
+});
+
+$(".underline-btn").click(function(){
+    var underlineButtons = Array.prototype.slice.call(document.getElementsByClassName("underline-btn"));
+    var index = underlineButtons.indexOf(this);
+    textElems[index].classList.toggle('underline-text');
+});
+
+var colorPickers = document.getElementsByClassName('color-picker');
+for(var i = 0; i<colorPickers.length; i++){
+    colorPickers[i].addEventListener("input", watchColorPicker);
+}
+
+function watchColorPicker() {
+    this.parentNode.style.backgroundColor = this.value;
+    var index = getIndexOfThis(this,colorPickers);
+    textElems[index].style.color = this.value;
+}
+
+var backColorPickers = document.getElementsByClassName('back-color-picker');
+for(var i = 0; i<backColorPickers.length; i++){
+    backColorPickers[i].addEventListener("input", watchBackColorPicker);
+}
+
+function watchBackColorPicker() {
+    this.parentNode.style.backgroundColor = this.value;
+    var index = getIndexOfThis(this,backColorPickers);
+    textElems[index].style.backgroundColor = this.value;
+}
+
+var cancelBtns = document.getElementsByClassName('cancel-btn');
+for(var i = 0; i<cancelBtns.length; i++){
+    cancelBtns[i].addEventListener("click", removeBackground);
+}
+
+function removeBackground() {
+    var index = getIndexOfThis(this,cancelBtns);
+    backColorPickers[index].parentNode.style.backgroundColor = "transparent";
+    textElems[index].style.backgroundColor = "transparent";
+}
+
+var rangeSliders = document.getElementsByClassName("myslider");
+var fonts = document.getElementsByClassName("font-size");
+for(var i = 0; i<rangeSliders.length; i++){
+    rangeSliders[i].addEventListener("input", changeFont);
+}
+
+function changeFont() {
+    var index = getIndexOfThis(this,rangeSliders);
+    fonts[index].innerHTML = this.value;
+    textElems[index].style.fontSize = ""+this.value+"px";
+}
+
+/************************************** IMAGE PANELS ***************************************/
+
+/* Initialize labels used to change image and its index (tbd when clicked) */
+var uploadBtns = document.getElementsByClassName("upload-icon");
+var uploadIndex;
+
+/* When image upload label is clicked, its index is determined */
+for(var i = 0; i<uploadBtns.length; i++){
+    uploadBtns[i].addEventListener("click", updateUploadIndex);
+}
+
+function updateUploadIndex() {
+    uploadIndex = getIndexOfThis(this, uploadBtns);
+}
+
+/* Intialize images on webpage and panels */
+var uploadImages = document.getElementsByClassName("upload-img");
+var pageImages = document.getElementsByClassName("page-img");
+
+/* Assigning event listener to "Save Changes" button on image upload modal */
+var changeImageBtn = document.getElementById("change-image");
+changeImageBtn.addEventListener("click", changeImage);
+
+/* Image input field */
+var imageInput = document.getElementById("image-input");
+
+/* Gets called to change image once "Save Changes" is clicked using label index */
+function changeImage() {
+    var url = imageInput.value;
+    var pattern = /^((http|https|ftp):\/\/)/;
+    if(!pattern.test(url)) {
+        url = "https://" + url;
+    }
+    uploadImages[uploadIndex].src = url;
+    pageImages[uploadIndex].src = url;
+}
+
+/* Add event listeners to image number input elements */
+var imgNums = document.getElementsByClassName("img-num");
+for(var i = 0; i<imgNums.length; i++){
+    imgNums[i].addEventListener("input", updateImageNum);
+}
+
+/* Update number of images shown on page and upload panels */
+var imageRows = document.getElementsByClassName("product-images");
+var uploadRows = document.getElementsByClassName("upload-row");
+
+var secondImage = [];
+var secondUploadImage = [];
+var thirdImage = [];
+var thirdUploadImage = [];
+thirdImage[1] = imageRows[1].removeChild(imageRows[1].lastElementChild);
+thirdUploadImage[1] = uploadRows[1].removeChild(uploadRows[1].lastElementChild);
+
+function updateImageNum() {
+    var index = getIndexOfThis(this,imgNums);
+    console.log(imageRows[index].childNodes);
+    if(this.value < imageRows[index].childElementCount) {
+        if(this.value == 2) {
+            console.log(imageRows[index].childElementCount);
+            thirdImage[index] = imageRows[index].removeChild(imageRows[index].lastElementChild);
+            thirdUploadImage[index] = uploadRows[index].removeChild(uploadRows[index].lastElementChild);
+            imageRows[index].lastElementChild.classList.remove("col-md-4");
+            imageRows[index].lastElementChild.classList.add("col-md-5");
+            imageRows[index].childNodes[1].classList.remove("col-md-4");
+            imageRows[index].childNodes[1].classList.add("col-md-5");
+        }
+        else if(this.value == 1) {
+            secondImage[index] = imageRows[index].removeChild(imageRows[index].lastElementChild);
+            secondUploadImage[index] = uploadRows[index].removeChild(uploadRows[index].lastElementChild);
+            imageRows[index].childNodes[1].classList.remove("col-md-5");
+            imageRows[index].childNodes[1].classList.add("col-md-6");
+        }
+    }
+    else if (this.value > imageRows[index].childElementCount){
+        if(this.value == 2) { 
+            imageRows[index].childNodes[1].classList.remove("col-md-6");
+            imageRows[index].childNodes[1].classList.add("col-md-5");
+            imageRows[index].appendChild(secondImage[index]);
+            uploadRows[index].appendChild(secondUploadImage[index]);
+        }
+        else if(this.value == 3) {
+            imageRows[index].childNodes[1].classList.remove("col-md-5");
+            imageRows[index].childNodes[1].classList.add("col-md-4");
+            console.log(imageRows[index].childNodes);
+            imageRows[index].lastElementChild.classList.add("col-md-4");
+            imageRows[index].lastElementChild.classList.remove("col-md-5");
+            imageRows[index].appendChild(thirdImage[index]);
+            uploadRows[index].appendChild(thirdUploadImage[index]);
+        }
+    }
+}
+  
+
+/***********************************************************************************************************************************************/
+function swap(array,index,newIndex){
+    var temp = array[index];
+    array[index] = array[newIndex];
+    array[newIndex] = temp;
+}
+
+/* Move sections up/down */
+$(".move-down").click(function() {
+    var index;
+    for (var i = 0; i < siteSections.length-1; i++) {
+        if(siteSections[i].id == this.name){
+            index = i;
+        }
+    }
+    swap(siteSections,index,index+1);
+    for (let item of siteSections) {
+        document.getElementById("section-container").appendChild(item);
+    }
+    swap(panelSections,index,index+1);
+    for (let item of panelSections) {
+        document.getElementById("accordion").appendChild(item);
+    }
+});
+
+$(".move-up").click(function() {
+    var index;
+    for (var i = 1; i < siteSections.length; i++) {
+        if(siteSections[i].id == this.name){
+            index = i;
+        }
+    } 
+    swap(siteSections,index,index-1);
+    for (let item of siteSections) {
+        document.getElementById("section-container").appendChild(item);
+    }
+    swap(panelSections,index,index-1);
+    for (let item of panelSections) {
+        document.getElementById("accordion").appendChild(item);
+    }
+});
+
+
+/************************************** Section Settings ***************************************/
+/* Updating header background image */
+$('#update-header-image').click(function() {
+    var imageUrl = document.getElementById("update-header-image-input").value;
+    var pattern = /^((http|https|ftp):\/\/)/;
+    if(!pattern.test(imageUrl)) {
+        imageUrl = "https://" + imageUrl;
+    }
+    $('#header').css("background-image", "url(" + imageUrl + ")");   
+});
+
+/* Updating section background colour */
+var sectionColor = document.getElementById('section-background-color-picker');
+sectionColor.addEventListener("input", watchSectionColorPicker);
+
+var sectionId;
+var settingIcons = document.getElementsByClassName("section-settings");
+for(var i = 1; i < 4; i++) {
+    settingIcons[i].addEventListener("click", setSectionId);
+}
+
+function setSectionId() {
+    sectionId = this.name;
+}
+
+function watchSectionColorPicker() {
+    this.parentNode.style.backgroundColor = this.value;
+}
+
+$("#update-section-color").click(function() {
+    console.log(sectionId);
+    $('#' + sectionId).css("background-color", sectionColor.value);
+});
+
+
+/************************************** Page Preview and Export ***************************************/
+/* Site code export */
+$('#export-btn').click(function () { 
+    var html = $('#section-container').html();
+    var head = "<head>\n<title>Website</title>\n<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>\n<link rel='stylesheet' type='text/css' href='https://emilyanas2323.github.io/assets/css/editor-site.css'>\n</head>\n";
+    $('#html-code').val("<html>\n" + head + "<body>" + html + "</body>\n</html>");
+});
+
+/* Copy to Clipboard */
+function copy() {
+    if(this.id == "copy-html") {
+        document.querySelector("#html-code").select();
+    }
+    document.execCommand("copy");
+    document.getElementById("copy-message").textContent = "The code has been copied!"
+}
+
+document.querySelector("#copy-html").addEventListener("click", copy);
+document.getElementById("dismiss-export").addEventListener("click", function() {
+    document.getElementById("copy-message").textContent = "";
+});
+
+
+/* Site preview in new tab */
+function newWindow_method_1() {
+    var html = $('#section-container').html();
+    var wi = window.open();
+    /*var headHTML = $('head').html();
+    $(wi.document.head).html(headHTML);*/
+    $(wi.document.head).append("<title>Website Preview</title>");
+    $(wi.document.head).append("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>");
+    $(wi.document.head).append("<link rel='stylesheet' type='text/css' href='https://emilyanas2323.github.io/assets/css/editor-site.css'>");
+    $(wi.document.body).html(html);
+  }
+  $('#preview-btn').on('click', function () { 
+    newWindow_method_1();
+});
